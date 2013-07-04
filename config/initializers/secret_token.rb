96c57ff91294404b28e926b903254287ad4741b7
Lillie Chilen <lillie.chilen@gmail.com>
2013-07-03 18:17:41 -0700

    Add Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Giftdriver::Application.config.secret_token = '09e3580eb00588ed7c5cb5d80de4b8f9834dc4e587da8be9a90cb681915f486d07141ff84415bd9b1748199a3c99f3286af99a071e511a9b6dbdc32bebfe940a'
