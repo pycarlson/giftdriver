3a2e6d79a83f662f984b2e74d0737b4b1d2d1db1
DBC-Apprentice <dbc@dbc.com>
2013-07-04 14:11:55 -0700

    working model/migrations

--- app/models/drive.rb
+++ app/models/drive.rb
@@ -1,4 +1,17 @@
 class Drive < ActiveRecord::Base
   belongs_to :user
-  validates :organizer_id, :organization_name, :organization_blurb, :drive_title, :drive_blurb, :start_date, :end_date, :presence => true
+
+  attr_accessible :org_blurb,
+                  :org_email,
+                  :org_phone,
+                  :org_name,
+                  :org_address,
+                  :org_zipcode,
+                  :drop_location,
+                  :drive_title,
+                  :drive_blurb,
+                  :start_date,
+                  :end_date
+
+  # validates :organizer_id, :organization_name, :organization_blurb, :drive_title, :drive_blurb, :start_date, :end_date, :presence => true
 end
