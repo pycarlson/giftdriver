81dd8ede0fad7a4d69faa0af9992938eb649b7e5
nathanielwroblewski <nathanielwroblewski@gmail.com>
2013-07-05 17:24:00 -0700

    Mysterious migration updates

--- db/migrate/20130705213736_add_family_code_column_to_families_table.rb
+++ db/migrate/20130705213736_add_family_code_column_to_families_table.rb
@@ -1,5 +1,5 @@
 class AddFamilyCodeColumnToFamiliesTable < ActiveRecord::Migration
   def change
-    add_column :families, :code, :string
+    add_column :families, :family_code, :string
   end
 end
--- db/migrate/20130705222446_rename_family_code.rb
+++ db/migrate/20130705222446_rename_family_code.rb
@@ -1,5 +1,5 @@
 class RenameFamilyCode < ActiveRecord::Migration
   def change
-    rename_column :families, :code, :code
+    rename_column :families, :family_code, :code
   end
 end
