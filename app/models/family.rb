6bfddfe796a5dd7ffb6eccb68b4952573d1ea46a
Lillie Chilen <lillie.chilen@gmail.com>
2013-07-05 23:11:00 -0700

    Show family member names and sample of needs on drive show page

--- app/models/family.rb
+++ app/models/family.rb
@@ -16,4 +16,15 @@ class Family < ActiveRecord::Base
     Drive.find(drive).families.where('adopted_by IS NOT NULL')
   end
 
+  def members_names_sentence
+    names = self.family_members.map { |member| member.first_name }
+    names.to_sentence
+  end
+
+  def sampled_needs(total_needs)
+    need_objects = self.family_members.map { |person| person.needs }
+    need_strings = need_objects.map { |x| x[0].text }
+    need_strings.sample(total_needs)
+  end
+
 end
