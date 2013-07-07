27d24c9648c01f42598e09e751b90b53cc4c419d
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-07 12:32:45 -0700

    Add error handling to create on needs controller

--- app/controllers/needs_controller.rb
+++ app/controllers/needs_controller.rb
@@ -7,15 +7,15 @@ class NeedsController < ApplicationController
   end
 
   def create
-    family_member = FamilyMember.find(params[:family_member_id])
-    need = Need.new(params[:need])
-    family_member.needs << need
+    @member = FamilyMember.find(params[:family_member_id])
+    @need = Need.new(params[:need])
+    @member.needs << @need
 
-    if need.save
-      redirect_to family_path(family_member.family_id)
+    if @need.save
+      redirect_to family_path(@member.family_id)
     else
-      flash[:notice] = "Need didn't save. Sorry!"
-      redirect_to family_path(family_member.family_id)
+      flash.now[:error] = @need.errors.full_messages
+      render :new
     end
   end
 
