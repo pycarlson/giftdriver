0e47d3aa4b52a1979f2e67c07335ba9b9c859139
dbc-warrior <apprentice@devbootcamp.com>
2013-07-07 13:53:11 -0700

    fixed error handling for family data import. Preliminary styling for  error alert table, which Lillie hates.

--- app/controllers/family_member_imports_controller.rb
+++ app/controllers/family_member_imports_controller.rb
@@ -5,8 +5,9 @@ class FamilyMemberImportsController < ApplicationController
   end
 
   def create
+    @drive = Drive.find(params[:drive_id])
     @family_members = FamilyMemberImport.new(params[:family_member_import], params[:drive_id])
-
+    
     if @family_members.save
       redirect_to root_path, notice: "Families imported successfully."
     else
