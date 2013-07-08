829a7629eb2dec963f9951f8df0d4ecfeb2ea0bd
Lillie Chilen <lillie.chilen@gmail.com>
2013-07-08 12:04:52 -0700

    Add location preference create method

--- app/controllers/donors_controller.rb
+++ app/controllers/donors_controller.rb
@@ -4,4 +4,13 @@ class DonorsController < ApplicationController
     @locations = @drive.drop_locations
     @donor = Donor.new
   end
+
+  def create
+    donor = Donor.new
+    donor.drive_id = params[:drive_id]
+    donor.user_id = current_user.id
+    donor.drop_location_id = params[:donor][:drop_location]
+    donor.save
+    redirect_to drive_path(params[:drive_id])
+  end
 end
