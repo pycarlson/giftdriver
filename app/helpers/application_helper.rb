56e257010b123965acc8aa0713b53e95de72cc54
= <=>
2013-07-09 15:07:03 -0700

    correct code after working with Shadi

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,7 @@ module ApplicationHelper
   def organizer?(drive)
     access = Organizer.where("user_id = ? AND drive_id = ?", current_user.id, drive.id).first
     true unless access.nil?
+    p "THIS IS THE PLACE I WANT TO BE!!!!!"
   end
 
   def last_organizer?(drive)
