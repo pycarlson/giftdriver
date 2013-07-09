cde7f2c64ca6e326600bf8b36965ffe256b4169e
Lillie Chilen <lillie.chilen@gmail.com>
2013-07-09 11:08:17 -0700

    Make users confirmable

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
+
 ActiveRecord::Schema.define(:version => 20130709173613) do
 
   create_table "donors", :force => true do |t|
@@ -96,8 +97,13 @@ ActiveRecord::Schema.define(:version => 20130709173613) do
     t.datetime "updated_at",                             :null => false
     t.string   "full_name"
     t.string   "company"
+    t.string   "unconfirmed_email"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
   end
 
+  add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
