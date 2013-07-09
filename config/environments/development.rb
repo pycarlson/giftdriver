a7e990398f62adc98fb54d2d4b65087524d86597
Lillie Chilen <lillie.chilen@gmail.com>
2013-07-09 11:02:50 -0700

    Set up ActionMailer in production & dev environments

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,9 +13,6 @@ Giftdriver::Application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = true
-
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
 
@@ -37,7 +34,12 @@ Giftdriver::Application.configure do
 
   config.action_mailer.perform_deliveries = true
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-  
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.raise_delivery_errors = true
+
+  # Use MailCatcher! It's not in the Gemfile - get it running locally with these commands:
+  # gem install mailcatcher
+  # mailcatcher
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = { :address => "127.0.0.1", :port => 1025 }
 end
