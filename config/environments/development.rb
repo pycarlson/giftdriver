f7d3f4782bff5914d4a98f70b2226014ad991743
Lillie Chilen <lillie.chilen@gmail.com>
2013-07-09 12:30:45 -0700

    Working toward functional email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,7 +32,7 @@ Giftdriver::Application.configure do
   # Expands the lines which load the assets
   config.assets.debug = true
 
-  config.action_mailer.perform_deliveries = true
+  config.action_mailer.perform_deliveries = false
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.default :charset => "utf-8"
   config.action_mailer.raise_delivery_errors = true
