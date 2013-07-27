b595d348776cc2daab69e2677261eba2112fa5eb
Paulette Luftig <pyluftig@gmail.com>
2013-07-26 20:56:34 -0700

    add styling and jquery to adoption confirmation box

--- app/assets/javascripts/confirmation.js
+++ app/assets/javascripts/confirmation.js
@@ -2,8 +2,16 @@ $(document).ready(function(){
   $('div #adoption-confirmation').hide();
   $('#adopt-button').on('click', function(e){
     e.preventDefault();
+    $('.add-family-button').hide();
+    $('#adopt-button').hide();
     $('.family-details-card').hide();
     $('div #adoption-confirmation').show();
-    console.log("made it here!");
+  });
+  $('#no').on('click', function(e){
+    e.preventDefault();
+    $('div #adoption-confirmation').hide();
+    $('.family-details-card').show();
+    $('#adopt-button').show();
+    $('.add-family-button').show();
   });
 });
