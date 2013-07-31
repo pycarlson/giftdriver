a6d941869be5a257f94bccb6336355f24f429d39
Lillie Chilen <lillie.chilen@gmail.com>
2013-07-30 18:38:13 -0700

    WIP Update filter by size JS to listen for submit * still not getting back the right thing

--- app/assets/javascripts/family_size_filter.js
+++ app/assets/javascripts/family_size_filter.js
@@ -1,22 +1,25 @@
 $(document).ready(function () {
-	hideAllFamilies = function() {
-		$('.family-details-card').hide();
-	}
-
+	
 	getFilteredFamilies = function(familySize) {
 		console.log("called getFilteredFamilies");
-		$('#filter-family-form').on("ajax:complete", function() {
-			console.log("The ajax call was complete");
+		$('#filter-family-form').on("ajax:success", function(event, data) {
+			console.log("getFilteredFamilies AJAX call");
+			console.log(data);
 		});
 	}
 
 	showFilteredFamilies = function (families) {
 		console.log("called showFilteredFamilies");
-		$('.families-to-adopt').html("<h3>Families go here</h3>");
+		$('.families-to-adopt').html("<h4>Families will go here</h4>");
 	}
 
+	hideAllFamilies = function() {
+		$('.family-details-card').hide();
+	}
+
+
 	filterFamilies = function() {
-		$('#family-size-filter-buttons').on('click', 'input', function(e) {
+		$('#family-size-filter-buttons input').last().on('click', function(e) {
 			var filterBySize = e.currentTarget.value;
 			console.log(filterBySize);
 			var families = getFilteredFamilies(filterBySize);
