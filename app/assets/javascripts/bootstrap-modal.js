244c97f4882f52ff5211886c331d20ed345077de
Paulette Luftig <pyluftig@gmail.com>
2013-09-06 21:26:47 -0700

    trouble shoot home page bug

--- app/assets/javascripts/bootstrap-modal.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-  $('#adoption-modal').modal('hide');
-  $('#adopt-button').on('click', function(e){
-    e.preventDefault();
-    $('#adoption-modal').modal('show');
-    
-  });
-  
-});
