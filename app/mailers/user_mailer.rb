88b6e10325449cf816eed23d94a98945b7391361
Paulette Luftig <pyluftig@gmail.com>
2013-10-28 20:01:53 -0700

    add dynamic drive / org return email address

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,12 +1,18 @@
 class UserMailer < ActionMailer::Base
-  default from: "lillie.chilen@gmail.com"
-
+  
+  default from: "pyluftig@gmail.com"
+  
   def adopted_family(user, family_id)
     @user = user
     @url  = 'http://example.com/login'
     @family = Family.find(family_id)
     @drive = Drive.find(@family.drive_id)
+    p "*" * 100
+    p @drive
+    p @family
     @location = DropLocation.find(@family.drop_location_id) if @family.drop_location_id
-    mail(to: @user.email, subject: 'Thank you from Giftdriver')
+    mail(to: @user.email, from: @drive.org_email, subject: 'Thank you from Giftdriver')
   end
+
+  
 end
